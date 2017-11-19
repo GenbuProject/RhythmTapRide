92f486ddb816970d47a80b4d2d95651362e088a1
Genbu Hase <hasegenbu@gmail.com>
2017-11-19 09:52:06 +0900

    スコアバー追加。

--- v3/assets/includes/RTR.js
+++ v3/assets/includes/RTR.js
@@ -38,14 +38,62 @@ class RTR {
 		}
 	}
 
-	static get Tone () {
-		return class Tone {
-			constructor () {
+	static get ToneStream () {
+		return class ToneStream {
+			constructor (deg = 0, speed = 2.5) {
+				this.x = RTR.self.width / 2,
+				this.y = RTR.self.height / 5;
 
+				this.deg = deg,
+				this.speed = speed;
 			}
 
-			renderer () {
-				
+			addTone () {
+				let tone = new RTR.Tone(this.x, this.y, 1),
+					dx = this.speed * Math.cos(DOM.util.degToRad(180 + this.deg)),
+					dy = -this.speed * Math.sin(DOM.util.degToRad(180 + this.deg));
+
+				let looper = setInterval(() => {
+					tone.x += dx,
+					tone.y += dy,
+					tone.radius += 250 / -(RTR.self.height - this.y) / dy;
+
+					tone.render();
+
+					if (tone.x < 0 || tone.x > RTR.self.width + tone.radius || tone.y > RTR.self.height + tone.radius) {
+						clearInterval(looper);
+					}
+				});
+			}
+		}
+	}
+
+	static get Tone () {
+		return class Tone {
+			constructor (x = 0, y = 0, radius = 10) {
+				this.x = x,
+				this.y = y,
+				this.radius = radius;
+
+				this.ringColor = "Plum";
+			}
+
+			render () {
+				let ctx = RTR.self.ctx;
+					ctx.beginPath();
+					ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+					ctx.closePath();
+
+					ctx.strokeStyle = this.ringColor;
+					ctx.stroke();
+			}
+		}
+	}
+
+	static get TapTone () {
+		return class TapTone extends RTR.Tone {
+			constructor (x = 0, y = 0, radius = 10) {
+				super(x, y, z);
 			}
 		}
 	}
@@ -61,10 +109,11 @@ class RTR {
 			get score () { return this._score }
 
 			set score (val = 0) {
-				this._score = val;
+				this._score = (val > 1000000 ? 1000000 : val);
 
 				this.self.setAttribute("score", this._score);
-				this.self.style.setProperty("--Score", this.score > 1000000 ? "100%" : `${this._score / 10000}%`);
+				this.self.style.setProperty("--Score", this._score > 1000000 ? '"1000000"' : `"${this._score}"`);
+				this.self.style.setProperty("--ScorePercentage", this._score > 1000000 ? "100%" : `${this._score / 10000}%`);
 			}
 		}
 	}
@@ -72,21 +121,33 @@ class RTR {
 
 
 	constructor () {
-		this.frameSize = Math.min(DOM.width, DOM.height);
+		let scorebar = this.scorebar = new RTR.Scorebar(0);
+			document.body.appendChild(scorebar.self);
 
-		this.cvs = DOM("Canvas", {
-			id: "PlayingLayout",
+		let cvs = this.cvs = DOM("Canvas", { id: "PlayingLayout" });
+			document.body.appendChild(cvs);
 
-			attributes: {
-				width: this.frameSize,
-				height: this.frameSize
-			},
 
-			styles: {
-				background: "Black"
-			}
-		});
 
-		document.body.appendChild(this.cvs);
+		this.width = DOM.width,
+		this.height = DOM.height - scorebar.self.offsetHeight;
+
+		this.ctx = this.cvs.getContext("2d");
+	}
+
+	get width () { return this._width }
+
+	set width (val = 0) {
+		this._width = val;
+
+		this.cvs.applyProperties({ attributes: { width: this._width } });
+	}
+
+	get height () { return this._height }
+
+	set height (val = 0) {
+		this._height = val;
+
+		this.cvs.applyProperties({ attributes: { height: this._height } });
 	}
 }
--- v3/main.js
+++ v3/main.js
@@ -1,1 +1,13 @@
-new RTR();
+let base = new RTR();
+
+window.addEventListener("resize", () => {
+	base.width = DOM.width,
+	base.height = DOM.height - base.scorebar.self.offsetHeight;
+
+	base.cvs.applyProperties({
+		attributes: {
+			width: base.width,
+			height: base.height
+		}
+	});
+})
